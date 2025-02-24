446e6ef965aa34e8a9113f065001a134e9851d41
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-02-24 08:33:08 +0100

    finished fetching user data from api

---
+++ fetch-data.js
@@ -0,0 +1,32 @@
+
+// Initializing my async funtion
+
+const fetchUserData = async () => {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        console.log(users);
+
+        dataContainer.innerHTML = "";
+
+        const userList = document.createElement("ul");
+
+        for (let user of users) {
+            const listItem = document.createElement("li");
+            listItem.textContent = user.name;
+            userList.appendChild(listItem);
+        }
+
+        dataContainer.appendChild(userList);
+        
+
+    } catch (error) {
+        dataContainer.textContent = "";
+        dataContainer.textContent = "Failed to load user data";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
